55fe5dba48b8b029454297f8a695ee52122cfc37
Omar Emad <64874084+OmarE505@users.noreply.github.com>
2023-11-23 18:54:52 +0200

    (feat): Add Drone Check
    Added scheduler for drone to check on it periodically.

---
+++ src/main/java/com/omarE505/DroneDelivery/Schedule/DroneCheck.java
@@ -0,0 +1,40 @@
+package com.omarE505.DroneDelivery.Schedule;
+
+import java.util.List;
+
+import com.omarE505.DroneDelivery.Repository.AuditDroneRepository;
+import com.omarE505.DroneDelivery.Repository.DroneRepository;
+import com.omarE505.DroneDelivery.entity.AuditDrone;
+import com.omarE505.DroneDelivery.entity.Drone;
+import com.omarE505.DroneDelivery.utils.State;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DroneCheck {
+
+    @Autowired
+    private DroneRepository droneRepository;
+
+    @Autowired
+    private AuditDroneRepository aDroneRepository;
+
+    @Scheduled(fixedDelay = 4 * 50 * 1000)
+    public void batteryCheck() {
+        List<Drone> drones = droneRepository.findAll();
+        drones.stream().forEach(drone -> {
+            try {
+                int batteryCapacity = drone.getBatteryCapacity();
+                State state = drone.getState();
+                AuditDrone aDrone = new AuditDrone(state, batteryCapacity);
+                aDrone.setDroneId(drone.getId());
+                aDroneRepository.save(aDrone);
+            } catch (IllegalArgumentException exc) {
+                throw new IllegalArgumentException("Drone not found");
+            }
+        });
+    }
+
+}
