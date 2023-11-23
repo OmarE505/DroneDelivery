d96715d48d0972288b9caf37dec83a2722a144f9
Omar Emad <64874084+OmarE505@users.noreply.github.com>
2023-11-23 17:42:29 +0200

    (feat): add sql data init.
    added a sql initialization for the data for api testing

---
+++ src/main/resources/data.sql
@@ -0,0 +1,25 @@
+INSERT INTO weight_model (id, name, weight_value) VALUES (1, 'LIGHT_WEIGHT', 100);
+INSERT INTO weight_model (id, name, weight_value) VALUES (2, 'MIDDLE_WEIGHT', 200);
+INSERT INTO weight_model (id, name, weight_value) VALUES (3, 'CRUISER_WEIGHT', 300);
+INSERT INTO weight_model (id, name, weight_value) VALUES (4, 'HEAVY_WEIGHT', 500);
+
+INSERT INTO drone VALUES (1, 100, 'IDLE', 1);
+INSERT INTO drone VALUES (2, 100, 'IDLE', 2);
+INSERT INTO drone VALUES (3, 100, 'IDLE', 3);
+INSERT INTO drone VALUES (4, 100, 'IDLE', 4);
+
+INSERT INTO serial_number VALUES (1, 'LO03X-KYWQG-VIOXE-QWTNL-AOKPF-MMAEC');
+INSERT INTO serial_number VALUES (2, 'BO0OS-IDPOI-V0F3N-IM1O1-P1FTO-MAYMK');
+INSERT INTO serial_number VALUES (3, 'XKGYC-NRC1W-CVUCF-1R0TJ-SDGPN-2V2YH');
+INSERT INTO serial_number VALUES (4, 'AP3MP-M2GIX-EODLV-TSBYW-XEQD1-CAIN1');
+
+INSERT INTO medication (id, code, name, weight) VALUES (1, '4K49RLHW4V4J', 'med1', 60);
+INSERT INTO medication (id, code, name, weight) VALUES (2, '39F3L2U2ME2VKR1', 'med2', 70);
+INSERT INTO medication (id, code, name, weight) VALUES (3, 'M3AYHO0TAC4', 'med3', 80);
+INSERT INTO medication (id, code, name, weight) VALUES (4, '1G73ZVZRD4U', 'med4', 100);
+INSERT INTO medication (id, code, name, weight) VALUES (5, 'U09ATHXR22TK', 'med5', 110);
+INSERT INTO medication (id, code, name, weight) VALUES (6, 'J41P3ZSK46D8', 'med6', 130);
+INSERT INTO medication (id, code, name, weight) VALUES (7, 'HC9H9Q8X0CF__', 'med7', 165);
+INSERT INTO medication (id, code, name, weight) VALUES (8, '302V7GDP9OJ_', 'med8', 146);
+INSERT INTO medication (id, code, name, weight) VALUES (9, '02ROPIB3218', 'med9', 210);
+INSERT INTO medication (id, code, name, weight) VALUES (10, 'D169G7RX585L', 'med10', 230);
